a8bf544c3e395cb445a241bf6909ec32ac36dea3
root <root@infra-main>
2024-10-06 19:27:27 +0300

    Roles and Updated playbooks

--- install_scripts/loki/install_go.sh
+++ install_scripts/loki/install_go.sh
@@ -6,6 +6,6 @@ wget https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
 
 tar -C /usr/local -xzf go*.tar.gz
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
 
 export PATH=$PATH:/usr/local/go/bin
